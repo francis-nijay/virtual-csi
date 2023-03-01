f70cb8b735cc710ecc06123153c58f093df40974
Nijay <Francis.Nijay@Dell.com>
2023-03-01 23:37:56 +0530

    Initial commit - virtual-csi

--- main.go
+++ main.go
@@ -11,10 +11,48 @@ import (
 
 // main is ignored when this package is built as a go plug-in.
 func main() {
+	driverName := flag.String("driver-name", "vcsi-driver.dellemc.com", "driver name")
+	driverSecret := flag.String("driver-secret", "/vcsi-driver-config/config", "driver secret file")
+	driverConfig := flag.String("driver-config", "/vcsi-driver-config-params/driver-config-params.yaml", "array config params")
+	enableLeaderElection := flag.Bool("leader-election", false, "boolean to enable leader election")
+
+	flag.Parse()
+
+	if *driverName == "" {
+		fmt.Fprintf(os.Stderr, "driver-name argument is mandatory")
+		os.Exit(1)
+	}
+	service.Name = *driverName
+
+	if *driverConfig == "" {
+		fmt.Fprintf(os.Stderr, "driver-config argument is mandatory")
+		os.Exit(1)
+	}
+	service.DriverConfig = *driverConfig
+
+	if *driverSecret == "" {
+		fmt.Fprintf(os.Stderr, "driver-secret argument is mandatory")
+		os.Exit(1)
+	}
+	service.DriverSecret = *driverSecret
+
+	if !*enableLeaderElection {
+		fmt.Println("leader election")
+	}
+
+	// Always set X_CSI_DEBUG to false irrespective of what user has specified
+	_ = os.Setenv(gocsi.EnvVarDebug, "false")
+	// We always want to enable Request and Response logging (no reason for users to control this)
+	_ = os.Setenv(gocsi.EnvVarReqLogging, "true")
+	_ = os.Setenv(gocsi.EnvVarRepLogging, "true")
+
+	service.DriverConfig = *driverConfig
+	service.DriverSecret = *driverSecret
+	
 	gocsi.Run(
 		context.Background(),
 		service.Name,
-		"A description of the SP",
+		"Dell vCSI Driver",
 		"",
 		provider.New())
 }
